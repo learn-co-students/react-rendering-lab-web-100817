385210e3e4c7449b10996a49e16a9dfadc04be85
github username <github email address>
2017-12-15 09:40:30 -0500

    Done.

--- test/root.js
+++ test/root.js
@@ -1,17 +1,22 @@
-require('babel-register')();
 
-var exposedProperties = ['window', 'navigator', 'document'];
+require("babel-register")();
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-var jsdom = require('jsdom').jsdom;
+configure({ adapter: new Adapter() });
 
-global.document = jsdom('');
+var exposedProperties = ["window", "navigator", "document"];
+
+var jsdom = require("jsdom").jsdom;
+
+global.document = jsdom("");
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
+Object.keys(document.defaultView).forEach(property => {
+  if (typeof global[property] === "undefined") {
     global[property] = document.defaultView[property];
   }
 });
 
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: "node.js"
 };
